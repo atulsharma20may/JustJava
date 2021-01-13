55228cad9994136ebae03dc91648b185b73428cf
Atul Sharma <atulsharma20may@gmail.com>
2021-01-13 13:58:56 +0530

    Updating Project

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.justjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void submitOrder(View view) {
+        EditText numberOfCoffeeTextView = findViewById(R.id.noOfCoffee);
+        TextView priceTextView = findViewById(R.id.textView3);
+
+        int numberOfCoffee = Integer.parseInt(numberOfCoffeeTextView.getText().toString());
+
+        double Amount = numberOfCoffee * 30;
+        String AmountToShow = Amount + "";
+        priceTextView.setText(AmountToShow);
     }
 }
